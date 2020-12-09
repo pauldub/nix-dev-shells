b2dfbc6db6cbee2bead0928c2756231a10279fd1
Paul d'Hubert <paul@tymate.com>
2020-12-09 09:24:19 +0100

    feat: add ruby devshell

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell }: rec {
+    shells = flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs) lib;
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
+        };
+
+        mkRubyShell = rubyPackage: bundlerPackage:
+          (lib.makeOverridable pkgs.mkDevShell {
+            packages = with pkgs; [
+              (rubyPackage.override { bundler = bundlerPackage; })
+              nodejs
+
+              pkgconfig
+              binutils
+              gcc
+              gnumake
+              libxml2.dev
+              libxslt.dev
+              zlib.dev
+              libffi.dev
+
+              postgresql_11
+            ];
+
+            bash.extra = ''
+              export PKG_CONFIG_PATH="$DEVSHELL_DIR/lib/pkgconfig"
+            '';
+          });
+      in { ruby = mkRubyShell pkgs.ruby pkgs.bundler; });
+    lib = { devShell = system: lang: shells."${lang}"."${system}"; };
+  };
+}
